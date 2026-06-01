ea209cc236dd3abdc3ae695504c2edec469df15a
Bill Avery <wravery@gmail.com>
2026-05-31 23:21:30 -0700

    fix: simplify and shrink resources module

--- update-bindings/src/resources.rs
+++ update-bindings/src/resources.rs
@@ -1,4 +1,4 @@
-use quote::{format_ident, quote};
+use convert_case::{Boundary, Case, Casing, Pattern};
 use std::{
     fmt::{self, Display, Formatter},
     fs,
@@ -28,6 +28,34 @@ enum Resource {
     String(String),
 }
 
+impl Resource {
+    fn cef_fn(&self) -> &str {
+        match self {
+            Resource::Control(_) => "cef_id_for_command_id_name",
+            Resource::Packed(_) => "cef_id_for_pack_resource_name",
+            Resource::String(_) => "cef_id_for_pack_string_name",
+        }
+    }
+
+    fn sys_name(&self) -> &str {
+        match self {
+            Resource::Control(name) | Resource::Packed(name) | Resource::String(name) => {
+                name.as_str()
+            }
+        }
+    }
+
+    fn name(&self) -> String {
+        self.sys_name()
+            .from_case(Case::Custom {
+                boundaries: &[Boundary::LowerUpper, Boundary::Acronym],
+                pattern: Pattern::Capital,
+                delimiter: "",
+            })
+            .to_case(Case::Constant)
+    }
+}
+
 #[derive(Default)]
 struct ParseTree {
     resources: Vec<Resource>,
@@ -35,87 +63,79 @@ struct ParseTree {
 
 impl ParseTree {
     pub fn write_prelude(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let header = quote! {
-            use cef_dll_sys::{
-                cef_id_for_command_id_name, cef_id_for_pack_resource_name, cef_id_for_pack_string_name,
-            };
-            use std::sync::OnceLock;
-        }
-        .to_string();
-        writeln!(f, "{header}")
+        writeln!(f, "use std::ffi::CStr;")
     }
 
     pub fn write_resources(&self, f: &mut Formatter<'_>) -> fmt::Result {
         for resource in &self.resources {
-            let name = match resource {
-                Resource::Control(name) | Resource::Packed(name) | Resource::String(name) => {
-                    name.as_str()
-                }
-            };
-            let Ok(name_lit) = syn::parse_str::<syn::LitCStr>(&format!(r#"c"{name}""#)) else {
-                continue;
-            };
-            let cef_fn = format_ident!(
-                "{}",
-                match resource {
-                    Resource::Control(_) => "cef_id_for_command_id_name",
-                    Resource::Packed(_) => "cef_id_for_pack_resource_name",
-                    Resource::String(_) => "cef_id_for_pack_string_name",
-                }
-            );
-            let name_ident = format_ident!("{}", name.to_lowercase());
+            let cef_fn = resource.cef_fn();
+            let sys_name = resource.sys_name();
+            let name = resource.name();
             writeln!(
                 f,
-                "\n/// Load the resource ID for [`{name}`] with [`{cef_fn}`]."
+                r#"
+/// `"{sys_name}"`: Resource ID for use with [`cef_dll_sys::{cef_fn}`].
+pub const {name}: &CStr = c"{sys_name}";"#
             )?;
-            let getter = quote! {
-                pub fn #name_ident() -> Option<i32> {
-                    static RESOURCE_ID: OnceLock<Option<i32>> = OnceLock::new();
-                    *RESOURCE_ID.get_or_init(|| {
-                        let resource_id = unsafe { #cef_fn(#name_lit.as_ptr()) };
-                        if resource_id == -1 {
-                            None
-                        } else {
-                            Some(resource_id)
-                        }
-                    })
-                }
-            }
-            .to_string();
-            writeln!(f, "{getter}")?;
         }
         Ok(())
     }
 
     pub fn write_tests(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let asserts = self.resources.iter().map(|resource| {
-            let name = match resource {
-                Resource::Control(name) | Resource::Packed(name) | Resource::String(name) => name,
-            };
-            let cef_fn = format_ident!("{}", name.to_lowercase());
-            let sys_id = format_ident!("{name}");
-            quote! {
-                if let Some(resource_id) = #cef_fn() {
-                    assert_eq!(resource_id, sys::#sys_id);
-                }
-            }
-        });
-        let tests = quote! {
-            #[cfg(test)]
-            mod test {
-                use super::*;
-                use crate::*;
+        writeln!(
+            f,
+            r#"
+#[cfg(test)]
+mod tests {{
+    use crate::*;
 
-                #[test]
-                fn test_id_mapping() {
-                    test_init_cef();
+    #[test]
+    fn test_resource_mapping() {{
+        test_init_cef();"#
+        )?;
 
-                    #(#asserts)*
-                }
-            }
+        if let Some(first_control) = self
+            .resources
+            .iter()
+            .find(|resource| matches!(resource, Resource::Control(_)))
+        {
+            Self::write_assert(f, first_control)?;
         }
-        .to_string();
-        write!(f, "\n{tests}")
+
+        if let Some(first_packed) = self
+            .resources
+            .iter()
+            .find(|resource| matches!(resource, Resource::Packed(_)))
+        {
+            Self::write_assert(f, first_packed)?;
+        }
+
+        if let Some(first_string) = self
+            .resources
+            .iter()
+            .find(|resource| matches!(resource, Resource::String(_)))
+        {
+            Self::write_assert(f, first_string)?;
+        }
+
+        writeln!(
+            f,
+            r#"    }}
+}}"#
+        )
+    }
+
+    fn write_assert(f: &mut Formatter<'_>, resource: &Resource) -> fmt::Result {
+        let cef_fn = resource.cef_fn();
+        let sys_name = resource.sys_name();
+        let name = resource.name();
+
+        writeln!(
+            f,
+            r#"
+    let resource_id = unsafe {{ sys::{cef_fn}(resources::{name}.as_ptr()) }};
+    assert_eq!(resource_id, sys::{sys_name});"#
+        )
     }
 }
 
@@ -129,39 +149,37 @@ impl Display for ParseTree {
 
 impl From<&syn::File> for ParseTree {
     fn from(value: &syn::File) -> Self {
-        let mut tree = Self::default();
-
-        tree.resources = value
-            .items
-            .iter()
-            .filter_map(|item| match item {
-                syn::Item::Const(syn::ItemConst {
-                    vis: syn::Visibility::Public(_),
-                    ident,
-                    ..
-                }) => {
-                    let name = ident.to_string();
-                    if name.starts_with("IDC_") {
-                        Some(Resource::Control(name))
-                    } else if name.starts_with("IDR_") {
-                        Some(Resource::Packed(name))
-                    } else if name.starts_with("IDS_") {
-                        Some(Resource::String(name))
-                    } else {
-                        None
+        Self {
+            resources: value
+                .items
+                .iter()
+                .filter_map(|item| match item {
+                    syn::Item::Const(syn::ItemConst {
+                        vis: syn::Visibility::Public(_),
+                        ident,
+                        ..
+                    }) => {
+                        let name = ident.to_string();
+                        if name.starts_with("IDC_") {
+                            Some(Resource::Control(name))
+                        } else if name.starts_with("IDR_") {
+                            Some(Resource::Packed(name))
+                        } else if name.starts_with("IDS_") {
+                            Some(Resource::String(name))
+                        } else {
+                            None
+                        }
                     }
-                }
-                _ => None,
-            })
-            .collect();
-
-        tree
+                    _ => None,
+                })
+                .collect(),
+        }
     }
 }
 
 fn format_bindings(source_path: &Path) -> crate::Result<()> {
-    let mut cmd = Command::new("cargo");
-    cmd.args(&["fmt", "--", &source_path.display().to_string()]);
+    let mut cmd = Command::new(env!("CARGO"));
+    cmd.args(["fmt", "--", &source_path.display().to_string()]);
     cmd.output()?;
     Ok(())
 }
